681a9751a5e3006899aa211356c486cdf292ac88
Jon Keating <jon@licq.org>
2023-10-21 13:46:10 -1000

    Initial commit

---
+++ lib/ui/dashboard.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:soundboard/ui/tabs/faq.dart';
+import 'package:soundboard/ui/tabs/home.dart';
+import 'package:soundboard/ui/tabs/settings.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          HomeScreen(),
+          SettingsScreen(),
+          FAQScreen(),
+        ],
+      ),
+      bottomNavigationBar: Material(
+        color: Theme.of(context).colorScheme.inversePrimary,
+        child: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.music_note),
+            ),
+            Tab(
+              icon: Icon(Icons.settings),
+            ),
+            Tab(
+              icon: Icon(Icons.question_mark),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/tabs/faq.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FAQScreen extends StatefulWidget {
+  const FAQScreen({super.key});
+
+  @override
+  State<FAQScreen> createState() => _FAQScreenState();
+}
+
+class _FAQScreenState extends State<FAQScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('FAQ Screen'));
+  }
+}
