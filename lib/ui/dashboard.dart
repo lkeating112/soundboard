ecec6d0b939e4961eafe749c3ce96bffced359ba
rika <rm2701@iolani.org>
2023-10-30 12:05:00 +0800

    Add onTap to Images in GridView

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:soundboard/ui/tabs/faq.dart';
 import 'package:soundboard/ui/tabs/home.dart';
--- lib/ui/tabs/home.dart
+++ lib/ui/tabs/home.dart
@@ -10,6 +10,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home Screen'));
+    var images = [
+      Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+      Image.network('https://www.dexerto.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/05/06/svarog-coolest-character-honkai-star-rail.jpg'),
+      Image.network('https://i.ytimg.com/vi/QSQwZlRMVAM/maxresdefault.jpg'),
+      Image.network('https://cdn.shopify.com/s/files/1/1796/6745/files/10_b8909457-fe50-48dd-9899-bd1d9724df98_800x.png?v=1696607773'),
+      Image.network('https://media.tenor.com/ixhbuYewXtsAAAAC/eggdog.gif')
+    ];
+
+    return GridView.count(crossAxisCount:5, children: images,);
   }
 }
